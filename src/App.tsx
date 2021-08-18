c94332e9966358f7d32759ad79f3ac58d58206fb
Ingvaar <19124383+ingvaar@users.noreply.github.com>
2021-08-19 01:02:36 +0200

    feature/login -> Base login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { createBrowserHistory } from "history";
+import {
+  Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import LoginPage from 'app/screens/LoginPage/LoginPage.screen';
+
+export const history = createBrowserHistory({ forceRefresh: true });
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router history={history}>
+        <Switch>
+          <Route exact path="/login" component={LoginPage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/interfaces/ILoginForm.interface.tsx
@@ -0,0 +1,4 @@
+export interface ILoginForm {
+	identifier: string,
+	password: string,
+}
