2383791ec12e431e9da762b8b95de3d1f6f71d9c
Ingvaar <19124383+ingvaar@users.noreply.github.com>
2021-08-21 14:47:48 +0200

    feature/login -> Updating packages + Adding api, auth and user services

---
+++ src/services/enums/EAuthErros.enum.ts
@@ -0,0 +1,7 @@
+export enum EAuthErrors {
+  "Request without email" = "Please provide an email.",
+  "Request without password" = "Please provide a password.",
+  "Email already registered" = "Email already registered.",
+  "Invalid email/password" = "Invalid email or password",
+  "User unauthorized" = "Wrong email or password.",
+}
