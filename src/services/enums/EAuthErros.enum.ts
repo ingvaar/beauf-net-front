1d9f6026f9fb6610dd33f3cea159e8e11379d8db
Ingvaar <19124383+ingvaar@users.noreply.github.com>
2021-08-22 18:13:34 +0200

    feature/login -> dockerization + updating auth service

--- src/services/enums/EAuthErros.enum.ts
+++ src/services/enums/EAuthErros.enum.ts
@@ -2,6 +2,6 @@ export enum EAuthErrors {
   "Request without email" = "Please provide an email.",
   "Request without password" = "Please provide a password.",
   "Email already registered" = "Email already registered.",
-  "Invalid email/password" = "Invalid email or password",
+  "Invalid identifier or password" = "Invalid identifier or password",
   "User unauthorized" = "Wrong email or password.",
 }
