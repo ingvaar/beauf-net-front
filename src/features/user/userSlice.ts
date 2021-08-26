619939cae1564b3b0c647f79b57ad454e73cb65b
Ingvaar <19124383+ingvaar@users.noreply.github.com>
2021-08-26 15:02:26 +0200

    feature/menubar -> Using redux to store user info

---
+++ src/features/user/userSlice.ts
@@ -0,0 +1,32 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "../store";
+
+import { IUser } from "interfaces/IUser.interface";
+
+const initialState: IUser = {
+	id: "",
+	username: "",
+	role: ""
+};
+
+export const userSlice = createSlice({
+	name: "user",
+	initialState,
+	reducers: {
+		updateUser: (state: IUser, action: PayloadAction<IUser | any>) => {
+			return { ...state, ...action.payload };
+		},
+		deleteUser: (state: IUser) => {
+			state = initialState;
+			return { ...state };
+		}
+	},
+});
+
+export const { updateUser, deleteUser } = userSlice.actions;
+
+export const selectUser = (state: RootState): IUser => state.user;
+export const selectUserID = (state: RootState): string => state.user.id;
+export const selectUserRole = (state: RootState): string => state.user.role;
+
+export default userSlice.reducer;
