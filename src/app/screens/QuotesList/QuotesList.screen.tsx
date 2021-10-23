9d08680bfbe3ac3e1844f514d695ea25fcf15d93
Ingvaar <19124383+ingvaar@users.noreply.github.com>
2021-10-23 02:42:03 +0200

    feature/quote-list -> Handling total quotes

--- src/app/screens/QuotesList/QuotesList.screen.tsx
+++ src/app/screens/QuotesList/QuotesList.screen.tsx
@@ -4,10 +4,18 @@ import { FC, useState } from "react";
 export const QuotesListPage: FC = () => {
 	const [page, setPage] = useState<number>(1);
 	const [perPage, setPerPage] = useState<number>(50);
+	const [total, setTotal] = useState<number>(0);
+
+	const handleTotal = (total: number) => {
+		setTotal(total);
+	}
 
 	return (
 		<div id="quotes-list-page" className="column">
-			<QuotesList page={page} perPage={perPage}/>
+			<QuotesList page={page} perPage={perPage} handleTotal={handleTotal} />
+			<p>Page: {page}</p>
+			<p>Per Page: {perPage}</p>
+			<p>Total: {total}</p>
 		</div>
 	);
 };
