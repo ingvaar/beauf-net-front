86a7bcd270dc0c86200c4e0d198f2b80e2e12fd0
Ingvaar <19124383+ingvaar@users.noreply.github.com>
2021-10-23 22:13:06 +0200

    feature/quote-list -> basic pagination

--- src/app/screens/QuotesList/QuotesList.screen.tsx
+++ src/app/screens/QuotesList/QuotesList.screen.tsx
@@ -1,17 +1,17 @@
+import { Pagination } from "app/components/Pagination/Pagination";
 import { QuotesList } from "app/components/QuotesList/QuotesList"
 import { FC, useState } from "react";
 
 export const QuotesListPage: FC = () => {
 	const [page, setPage] = useState<number>(1);
-	const [perPage, setPerPage] = useState<number>(50);
+	const [perPage, setPerPage] = useState<number>(1);
 	const [total, setTotal] = useState<number>(0);
 
 	return (
 		<div id="quotes-list-page" className="column">
 			<QuotesList page={page} perPage={perPage} setTotal={setTotal} />
-			<p>Page: {page}</p>
+			<Pagination total={total} page={page} perPage={perPage} setPage={setPage} />
 			<p>Per Page: {perPage}</p>
-			<p>Total: {total}</p>
 		</div>
 	);
 };
