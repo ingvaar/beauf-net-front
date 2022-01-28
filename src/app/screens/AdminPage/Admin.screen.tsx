a7c474467a49b8d7bf89a4123c1a346b42be20d6
Ingvaar <19124383+ingvaar@users.noreply.github.com>
2022-01-28 06:26:11 +0100

    fix/quotes-validation -> admin panel option available only for admin + protection on admin page if not admin

--- src/app/screens/AdminPage/Admin.screen.tsx
+++ src/app/screens/AdminPage/Admin.screen.tsx
@@ -3,11 +3,17 @@ import { ButtonTrash } from "app/components/ButtonTrash/ButtonTrash";
 import { Pagination } from "app/components/Pagination/Pagination";
 import { PerPage } from "app/components/PerPage/PerPage";
 import { TrashModal } from "app/components/TrashModal/TrashModal";
+import { selectUser } from "features/user/userSlice";
+import { useAppSelector } from "hooks";
+import { IUser } from "interfaces/IUser.interface";
 import { FC, useState } from "react";
+import { useHistory } from "react-router-dom";
 
 import "./scss/Admin.scss";
 
 export const AdminPage: FC = () => {
+	const user: IUser = useAppSelector(selectUser);
+	const history = useHistory();
 	const [page, setPage] = useState<number>(1);
 	const [perPage, setPerPage] = useState<number>(10);
 	const [total, setTotal] = useState<number>(0);
@@ -21,8 +27,15 @@ export const AdminPage: FC = () => {
 		setTrashModalOpen(true);
 	}
 
+	const checkUser = () => {
+		if (!(user.role.length > 0 && user.role === 'admin')) {
+			history.push("/");
+		}
+	}
+
 	return (
 		<div id="admin-page" className="column">
+			{checkUser()}
 			<ButtonTrash openModal={openModal} />
 			<PerPage perPage={perPage} setPerPage={setPerPage} />
 			<AdminQuotesList page={page} perPage={perPage} setTotal={setTotal} />
