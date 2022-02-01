0213ea06ea30da07a8df272ad451b4d1ed35faae
Ingvaar <19124383+ingvaar@users.noreply.github.com>
2022-02-01 02:08:29 +0100

    feature/user-creation -> add form validation on user creation

--- src/app/components/UserCreationForm/UserCreationForm.tsx
+++ src/app/components/UserCreationForm/UserCreationForm.tsx
@@ -1,7 +1,7 @@
 import { Fab, TextField, Button } from "@material-ui/core";
 import { KeyboardReturn } from "@material-ui/icons";
 import { INewUserForm } from "interfaces/INewUserForm.interface";
-import { FC, useState, useRef, FormEvent } from "react";
+import { FC, useState, useRef, FormEvent, useEffect } from "react";
 import ReCAPTCHA from "react-google-recaptcha";
 import { useHistory } from "react-router-dom";
 import { CDefaultNewUserForm } from "./constants/DefaultUserCreationForm.constant";
@@ -15,8 +15,73 @@ export const UserCreationForm: FC = () => {
 		CDefaultNewUserForm
 	);
 	const [posted, setPosted] = useState<boolean>(false);
+	// Password check
+	const [passwordCheckError, setPasswordCheckError] = useState<string>("");
+	const [passwordCheck, setPasswordCheck] = useState<string>("");
+	// Email check
+	const [emailCheckError, setEmailCheckError] = useState<string>("");
+	const [emailCheck, setEmailCheck] = useState<string>("");
+	// Form errors
+	const [emailError, setEmailError] = useState<string>("");
+	const [usernameError, setUsernameError] = useState<string>("");
+	const [passwordError, setPasswordError] = useState<string>("");
 
 	const recaptchaRef = useRef<ReCAPTCHA>(null);
+
+	// Password check
+	useEffect(() => {
+		if (passwordCheck.length > 0 && passwordCheck !== form.password) {
+			setPasswordCheckError("Password don't match");
+		} else {
+			setPasswordCheckError("")
+		}
+	}, [passwordCheck, passwordCheckError, form.password]);
+
+	// Password check error
+	useEffect(() => {
+		if (emailCheck.length > 0 && emailCheck !== form.email) {
+			setEmailCheckError("Email don't match");
+		} else {
+			setEmailCheckError("");
+		}
+	}, [emailCheck, emailCheckError, form.email]);
+
+	// Username validation
+	useEffect(() => {
+		const usernameRegexp = new RegExp('^\\w+$', 'i')
+
+		if (form.username.length > 0 && (form.username.length < 4 || form.username.length > 24)) {
+			setUsernameError("Username should be between 4 and 24 long");
+		} else if (form.username.length > 0 && !usernameRegexp.test(form.username)) {
+			setUsernameError("Username should only contains letters and numbers");
+		} else {
+			setUsernameError("");
+		}
+	}, [form.username, usernameError]);
+
+	// Email validation
+	useEffect(() => {
+		const emailRegexp = new RegExp('^\\S+[@]\\S+[.][a-z]+$', 'i')
+
+		if (form.email.length > 0 && !emailRegexp.test(form.email)) {
+			setEmailError("Invalid email");
+		} else {
+			setEmailError("");
+		}
+	}, [form.email, emailError]);
+
+	// Password validation
+	useEffect(() => {
+		const passwordRegexp = new RegExp('^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z])(?=.*[!@#$&\\*_\'\\-"èé`àç()[\\]?]).{0,}$', '')
+
+		if (form.password.length > 0 && (form.password.length < 8 || form.password.length > 32)) {
+			setPasswordError("Password should be between 8 and 32 long");
+		} else if (form.password.length > 0 && !passwordRegexp.test(form.password)) {
+			setPasswordError("Password should contains atleast one special character, one uppercase and one number");
+		} else {
+			setPasswordError("");
+		}
+	}, [form.password, passwordError]);
 
 	function handleChange(event: any) {
 		setError("");
@@ -38,22 +103,6 @@ export const UserCreationForm: FC = () => {
 		}
 	};
 
-	const checkEmail = (event: any) => {
-		if (event.target.value.length > 0 && event.target.value !== form.email) {
-			setError("Email dont match");
-		} else {
-			setError("");
-		}
-	}
-
-	const checkPassword = (event: any) => {
-		if (event.target.value.length > 0 && event.target.value !== form.password) {
-			setError("Password dont match");
-		} else {
-			setError("");
-		}
-	}
-
 	const submittedUserBody = (
 			<div className="submitted-user-body">
 				<h2 id="submitted-user-title">Thanks for submitting !</h2>
@@ -71,44 +120,51 @@ export const UserCreationForm: FC = () => {
 			<h2 id="new-user-form-title">Please provide some information</h2>
 			<form onSubmit={handleSubmit} className="new-user-form">
 				<TextField
-					id="standard"
 					label="Username"
 					name="username"
 					onChange={handleChange}
 					value={form.username}
 					variant="outlined"
+					error={usernameError.length > 0}
+					helperText={usernameError}
 				/>
 				<TextField
-					id="standard"
 					label="Email"
 					name="email"
 					onChange={handleChange}
 					value={form.email}
 					variant="outlined"
+					error={emailError.length > 0}
+					helperText={emailError}
 				/>
 				<TextField
-					id="standard"
 					label="Confirm email"
 					name="confirm-email"
 					variant="outlined"
-					onChange={checkEmail}
+					error={emailCheck.length > 0 && emailCheck !== form.email}
+					onChange={(e) => setEmailCheck(e.target.value)}
+					value={emailCheck}
+					helperText={emailCheckError}
 				/>
 				<TextField
-					id="standard"
 					label="Password"
 					name="password"
 					type="password"
 					onChange={handleChange}
 					value={form.password}
 					variant="outlined"
+					error={passwordError.length > 0}
+					helperText={passwordError}
 				/>
 				<TextField
-					id="standard"
 					label="Confirm password"
 					name="confirm-password"
 					type="password"
 					variant="outlined"
-					onChange={checkPassword}
+					error={passwordCheck.length > 0 && passwordCheck !== form.password}
+					onChange={(e) => setPasswordCheck(e.target.value)}
+					value={passwordCheck}
+					helperText={passwordCheckError}
 				/>
 				<ReCAPTCHA
 					ref={recaptchaRef}
